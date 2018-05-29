bf7734ba3bc03aaa4b2029f88824286eee5206b7
Sharon Mun <sharonymun@gmail.com>
2018-05-29 21:02:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,16 +15,17 @@ WIN_COMBINATIONS = [
       [2,4,6]
       ]
       
-       def display_board(board)
-        puts " #{board[0]} | #{board[1]} | #{board[2]} "
-        puts "-----------"
-        puts " #{board[3]} | #{board[4]} | #{board[5]} "
-        puts "-----------"
-        puts " #{board[6]} | #{board[7]} | #{board[8]} "
-    end
-    display_board = TicTacToe.new
-    display_board.@board #doesn't work. try again
+   def display_board(board)
+      puts " #{board[0]} | #{board[1]} | #{board[2]} "
+      puts "-----------"
+      puts " #{board[3]} | #{board[4]} | #{board[5]} "
+      puts "-----------"
+      puts " #{board[6]} | #{board[7]} | #{board[8]} "
+   end
+    
+    
       
-    end
+      
+end
       
       
