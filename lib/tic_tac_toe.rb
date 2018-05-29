e2b6c3561df510b2bca122595857066d20523deb
Sharon Mun <sharonymun@gmail.com>
2018-05-29 21:27:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,6 +62,19 @@ WIN_COMBINATIONS = [
        return "O"
       end
    end
+   def turn
+        puts "Please enter 1-9:"
+          input = gets.strip
+          index = input_to_index(input)
+        if valid_move?(index)
+          move(index, current_player)
+          display_board
+        else
+          turn
+      end
+   end
+   
+   
 end
       
       
